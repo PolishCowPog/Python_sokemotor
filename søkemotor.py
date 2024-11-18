faa91c8d649370b1fc54fab013736e7727e4bb68
PolishCowPog <156892491+PolishCowPog@users.noreply.github.com>
2024-11-18 10:43:42 +0100

    Updated code to search word, make list, count in list

--- søkemotor.py
+++ søkemotor.py
@@ -1,6 +1,47 @@
-f = open("tekstfil1.txt")
-search = input("søk etter linje: ")
+import os
 
-for line in f:
-    if search in line:
-        print(line)
+def print_menu():
+    os.system('cls')
+    print("1. Search line")
+    print("2. Search word")
+    print("3. Change file (Not implemented)")
+    choice = input("choose number from menu: ")
+    if choice == "1":
+        search_line()
+    elif choice == "2":
+        search_word()
+    else:
+        print_menu()
+
+
+
+def search_line():
+    os.system('cls')
+    f = open("tekstfil1.txt", "r")
+    search = input("search line in file: ")
+
+    for line in f:
+        if search in line:
+            print(line)
+    
+    input("Press any key to exit: ")
+    print_menu()
+
+def search_word():
+    os.system('cls')
+    f = open("tekstfil1.txt", "r")
+    search = input("Search word in file: ")
+    word_list = []
+
+    for line in f:
+        if search in line:
+            word_list.append(search)
+    print(word_list)
+    x = word_list.count(search)
+    print("choosen word was found " + str(x) + " time(s)")
+    
+    input("Press any key to exit: ")
+    print_menu()
+
+
+print_menu()
