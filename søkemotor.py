1e4ee253840c7c8f9b1dd81b9c83525782adaadf
PolishCowPog <156892491+PolishCowPog@users.noreply.github.com>
2024-11-18 10:55:04 +0100

    Made if statement to check current file

--- søkemotor.py
+++ søkemotor.py
@@ -20,15 +20,16 @@ def search_line():
     os.system('cls')
     
     #Tekst file 1
-    f = open("tekstfil1.txt", "r")
-    search = input("search line in file: ")
+    if current_file == "file1":
+        f = open("tekstfil1.txt", "r")
+        search = input("search line in file: ")
 
-    for line in f:
-        if search in line:
-            print(line)
-    
-    input("Press any key to exit: ")
-    print_menu()
+        for line in f:
+            if search in line:
+                print(line)
+        
+        input("Press any key to exit: ")
+        print_menu()
 
 
 #Search for word in file
@@ -36,23 +37,24 @@ def search_word():
     os.system('cls')
     
     #Tekst file 1
-    f = open("tekstfil1.txt", "r")
-    search = input("Search word in file: ")
-    word_list = [] #Empty list of found searched words
+    if current_file == "file1":
+        f = open("tekstfil1.txt", "r")
+        search = input("Search word in file: ")
+        word_list = [] #Empty list of found searched words
 
-    for line in f:
-        if search in line: #Checks if search is in any line in the file
-            word_list.append(search) #Adds each found search into the list
-    print(word_list) #Prints list
-    x = word_list.count(search) #Counts for search amount of times in list
-    print("choosen word was found " + str(x) + " time(s)") #Prints amount of time search was found
-    if word_list.count(search) >= 1: #If count in list equals 1 or more sets variable to true
-        word_found = True
-    else: #Else false
-        word_found = False
-    print("Word found = " + str(word_found)) #Prints out the variable
-    
-    input("Press any key to exit: ")
+        for line in f:
+            if search in line: #Checks if search is in any line in the file
+                word_list.append(search) #Adds each found search into the list
+        print(word_list) #Prints list
+        x = word_list.count(search) #Counts for search amount of times in list
+        print("choosen word was found " + str(x) + " time(s)") #Prints amount of time search was found
+        if word_list.count(search) >= 1: #If count in list equals 1 or more sets variable to true
+            word_found = True
+        else: #Else false
+            word_found = False
+        print("Word found = " + str(word_found)) #Prints out the variable
+        
+        input("Press any key to exit: ")
     print_menu()
 
 
