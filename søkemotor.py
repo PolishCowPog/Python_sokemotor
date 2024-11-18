7d494d102be3fb452a54084bbba5c963edffde64
PolishCowPog <156892491+PolishCowPog@users.noreply.github.com>
2024-11-18 10:51:40 +0100

    added comments and True/False for found word

--- søkemotor.py
+++ søkemotor.py
@@ -1,4 +1,5 @@
 import os
+current_file = "file1"
 
 def print_menu():
     os.system('cls')
@@ -14,9 +15,11 @@ def print_menu():
         print_menu()
 
 
-
+#Search for line in file
 def search_line():
     os.system('cls')
+    #Tekst file 1
+
     f = open("tekstfil1.txt", "r")
     search = input("search line in file: ")
 
@@ -27,21 +30,33 @@ def search_line():
     input("Press any key to exit: ")
     print_menu()
 
+
+#Search for word in file
 def search_word():
     os.system('cls')
+    #Tekst file 1
+
     f = open("tekstfil1.txt", "r")
     search = input("Search word in file: ")
-    word_list = []
+    word_list = [] #Empty list of found searched words
 
     for line in f:
-        if search in line:
-            word_list.append(search)
-    print(word_list)
-    x = word_list.count(search)
-    print("choosen word was found " + str(x) + " time(s)")
+        if search in line: #Checks if search is in any line in the file
+            word_list.append(search) #Adds each found search into the list
+    print(word_list) #Prints list
+    x = word_list.count(search) #Counts for search amount of times in list
+    print("choosen word was found " + str(x) + " time(s)") #Prints amount of time search was found
+    if word_list.count(search) >= 1: #If count in list equals 1 or more sets variable to true
+        word_found = True
+    else: #Else false
+        word_found = False
+    print("Word found = " + str(word_found)) #Prints out the variable
     
     input("Press any key to exit: ")
     print_menu()
+
+
+
 
 
 print_menu()
